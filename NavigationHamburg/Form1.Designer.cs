7495d786af3c4d6305360662fd0d9be69101e05c
Ng Kok Wah <kokwah98@gmail.com>
2017-08-30 23:28:20 +0800

    Add files via upload

---
+++ NavigationHamburg/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace NavigationHamburg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Spanel = new System.Windows.Forms.Panel();
+            this.btnPage52 = new System.Windows.Forms.Button();
+            this.btnPage42 = new System.Windows.Forms.Button();
+            this.btnPage62 = new System.Windows.Forms.Button();
+            this.btnPage32 = new System.Windows.Forms.Button();
+            this.btnPage22 = new System.Windows.Forms.Button();
+            this.btnPage12 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPage21 = new System.Windows.Forms.Button();
+            this.btnPage51 = new System.Windows.Forms.Button();
+            this.btnPage41 = new System.Windows.Forms.Button();
+            this.btnPage31 = new System.Windows.Forms.Button();
+            this.btnPage61 = new System.Windows.Forms.Button();
+            this.btnPage11 = new System.Windows.Forms.Button();
+            this.btncloseandopen = new System.Windows.Forms.Button();
+            this.Spanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Spanel
+            // 
+            this.Spanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Spanel.Controls.Add(this.btnPage52);
+            this.Spanel.Controls.Add(this.btnPage42);
+            this.Spanel.Controls.Add(this.btnPage62);
+            this.Spanel.Controls.Add(this.btnPage32);
+            this.Spanel.Controls.Add(this.btnPage22);
+            this.Spanel.Controls.Add(this.btnPage12);
+            this.Spanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Spanel.Location = new System.Drawing.Point(53, 0);
+            this.Spanel.Name = "Spanel";
+            this.Spanel.Size = new System.Drawing.Size(200, 557);
+            this.Spanel.TabIndex = 4;
+            // 
+            // btnPage52
+            // 
+            this.btnPage52.BackColor = System.Drawing.Color.Black;
+            this.btnPage52.ForeColor = System.Drawing.Color.White;
+            this.btnPage52.Location = new System.Drawing.Point(0, 412);
+            this.btnPage52.Name = "btnPage52";
+            this.btnPage52.Size = new System.Drawing.Size(200, 51);
+            this.btnPage52.TabIndex = 11;
+            this.btnPage52.Text = "Page5";
+            this.btnPage52.UseVisualStyleBackColor = false;
+            this.btnPage52.Click += new System.EventHandler(this.btnPage52_Click);
+            // 
+            // btnPage42
+            // 
+            this.btnPage42.BackColor = System.Drawing.Color.Black;
+            this.btnPage42.ForeColor = System.Drawing.Color.White;
+            this.btnPage42.Location = new System.Drawing.Point(0, 337);
+            this.btnPage42.Name = "btnPage42";
+            this.btnPage42.Size = new System.Drawing.Size(200, 51);
+            this.btnPage42.TabIndex = 10;
+            this.btnPage42.Text = "Page4";
+            this.btnPage42.UseVisualStyleBackColor = false;
+            this.btnPage42.Click += new System.EventHandler(this.btnPage42_Click);
+            // 
+            // btnPage62
+            // 
+            this.btnPage62.BackColor = System.Drawing.Color.Black;
+            this.btnPage62.ForeColor = System.Drawing.Color.White;
+            this.btnPage62.Location = new System.Drawing.Point(0, 480);
+            this.btnPage62.Name = "btnPage62";
+            this.btnPage62.Size = new System.Drawing.Size(200, 51);
+            this.btnPage62.TabIndex = 9;
+            this.btnPage62.Text = "Page6";
+            this.btnPage62.UseVisualStyleBackColor = false;
+            this.btnPage62.Click += new System.EventHandler(this.btnPage62_Click);
+            // 
+            // btnPage32
+            // 
+            this.btnPage32.BackColor = System.Drawing.Color.Black;
+            this.btnPage32.ForeColor = System.Drawing.Color.White;
+            this.btnPage32.Location = new System.Drawing.Point(0, 264);
+            this.btnPage32.Name = "btnPage32";
+            this.btnPage32.Size = new System.Drawing.Size(200, 51);
+            this.btnPage32.TabIndex = 8;
+            this.btnPage32.Text = "Page3";
+            this.btnPage32.UseVisualStyleBackColor = false;
+            this.btnPage32.Click += new System.EventHandler(this.btnPage32_Click);
+            // 
+            // btnPage22
+            // 
+            this.btnPage22.BackColor = System.Drawing.Color.Black;
+            this.btnPage22.ForeColor = System.Drawing.Color.White;
+            this.btnPage22.Location = new System.Drawing.Point(0, 180);
+            this.btnPage22.Name = "btnPage22";
+            this.btnPage22.Size = new System.Drawing.Size(200, 53);
+            this.btnPage22.TabIndex = 6;
+            this.btnPage22.Text = "Page2";
+            this.btnPage22.UseVisualStyleBackColor = false;
+            this.btnPage22.Click += new System.EventHandler(this.btnPage22_Click);
+            // 
+            // btnPage12
+            // 
+            this.btnPage12.BackColor = System.Drawing.Color.Black;
+            this.btnPage12.ForeColor = System.Drawing.Color.White;
+            this.btnPage12.Location = new System.Drawing.Point(0, 109);
+            this.btnPage12.Name = "btnPage12";
+            this.btnPage12.Size = new System.Drawing.Size(200, 49);
+            this.btnPage12.TabIndex = 7;
+            this.btnPage12.Text = "Page1";
+            this.btnPage12.UseVisualStyleBackColor = false;
+            this.btnPage12.Click += new System.EventHandler(this.btnPage12_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.btnPage21);
+            this.panel1.Controls.Add(this.btnPage51);
+            this.panel1.Controls.Add(this.btnPage41);
+            this.panel1.Controls.Add(this.btnPage31);
+            this.panel1.Controls.Add(this.btnPage61);
+            this.panel1.Controls.Add(this.btnPage11);
+            this.panel1.Controls.Add(this.btncloseandopen);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(53, 557);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnPage21
+            // 
+            this.btnPage21.Location = new System.Drawing.Point(0, 180);
+            this.btnPage21.Name = "btnPage21";
+            this.btnPage21.Size = new System.Drawing.Size(53, 53);
+            this.btnPage21.TabIndex = 7;
+            this.btnPage21.UseVisualStyleBackColor = true;
+            this.btnPage21.Click += new System.EventHandler(this.btnPage21_Click);
+            // 
+            // btnPage51
+            // 
+            this.btnPage51.Location = new System.Drawing.Point(0, 412);
+            this.btnPage51.Name = "btnPage51";
+            this.btnPage51.Size = new System.Drawing.Size(53, 51);
+            this.btnPage51.TabIndex = 12;
+            this.btnPage51.UseVisualStyleBackColor = true;
+            this.btnPage51.Click += new System.EventHandler(this.btnPage51_Click);
+            // 
+            // btnPage41
+            // 
+            this.btnPage41.Location = new System.Drawing.Point(0, 337);
+            this.btnPage41.Name = "btnPage41";
+            this.btnPage41.Size = new System.Drawing.Size(53, 51);
+            this.btnPage41.TabIndex = 11;
+            this.btnPage41.UseVisualStyleBackColor = true;
+            this.btnPage41.Click += new System.EventHandler(this.btnPage41_Click);
+            // 
+            // btnPage31
+            // 
+            this.btnPage31.Location = new System.Drawing.Point(0, 264);
+            this.btnPage31.Name = "btnPage31";
+            this.btnPage31.Size = new System.Drawing.Size(53, 51);
+            this.btnPage31.TabIndex = 9;
+            this.btnPage31.UseVisualStyleBackColor = true;
+            this.btnPage31.Click += new System.EventHandler(this.btnPage31_Click);
+            // 
+            // btnPage61
+            // 
+            this.btnPage61.Location = new System.Drawing.Point(0, 480);
+            this.btnPage61.Name = "btnPage61";
+            this.btnPage61.Size = new System.Drawing.Size(53, 51);
+            this.btnPage61.TabIndex = 10;
+            this.btnPage61.UseVisualStyleBackColor = true;
+            this.btnPage61.Click += new System.EventHandler(this.btnPage61_Click);
+            // 
+            // btnPage11
+            // 
+            this.btnPage11.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnPage11.Location = new System.Drawing.Point(0, 109);
+            this.btnPage11.Name = "btnPage11";
+            this.btnPage11.Size = new System.Drawing.Size(53, 49);
+            this.btnPage11.TabIndex = 8;
+            this.btnPage11.UseVisualStyleBackColor = true;
+            this.btnPage11.Click += new System.EventHandler(this.btnPage11_Click);
+            // 
+            // btncloseandopen
+            // 
+            this.btncloseandopen.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btncloseandopen.Image = ((System.Drawing.Image)(resources.GetObject("btncloseandopen.Image")));
+            this.btncloseandopen.Location = new System.Drawing.Point(0, 0);
+            this.btncloseandopen.Name = "btncloseandopen";
+            this.btncloseandopen.Size = new System.Drawing.Size(53, 54);
+            this.btncloseandopen.TabIndex = 0;
+            this.btncloseandopen.UseVisualStyleBackColor = true;
+            this.btncloseandopen.Click += new System.EventHandler(this.btncloseandopen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 557);
+            this.Controls.Add(this.Spanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Spanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Spanel;
+        private System.Windows.Forms.Button btnPage52;
+        private System.Windows.Forms.Button btnPage42;
+        private System.Windows.Forms.Button btnPage62;
+        private System.Windows.Forms.Button btnPage32;
+        private System.Windows.Forms.Button btnPage22;
+        private System.Windows.Forms.Button btnPage12;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPage21;
+        private System.Windows.Forms.Button btnPage51;
+        private System.Windows.Forms.Button btnPage41;
+        private System.Windows.Forms.Button btnPage31;
+        private System.Windows.Forms.Button btnPage61;
+        private System.Windows.Forms.Button btnPage11;
+        private System.Windows.Forms.Button btncloseandopen;
+    }
+}
+
---
+++ NavigationHamburg/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavigationHamburg
+{
+    public partial class Form1 : Form
+    {
+        private bool Hidden;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btncloseandopen.Image = Image.FromFile(@"..\..\Images\open.jpg");
+            Hidden = true;
+            Spanel.Width = 0;
+        }
+
+        private void btnPage11_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage12_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage21_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage22_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage31_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage32_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage41_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage42_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage51_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage52_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage61_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btnPage62_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TestPage win = new TestPage();
+            win.Show();
+        }
+
+        private void btncloseandopen_Click(object sender, EventArgs e)
+        {
+            if (Hidden == true)
+            {
+                btncloseandopen.Image = Image.FromFile(@"..\..\Images\close.jpg");
+                Spanel.Width = 150;
+                //panel3.Width = 700;
+                Hidden = false;
+            }
+            else
+            {
+                btncloseandopen.Image = Image.FromFile(@"..\..\Images\open.jpg");
+                Spanel.Width = 0;
+                //panel3.Width = 0;
+                Hidden = true;
+            }
+        }
+    }
+}
